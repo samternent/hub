f601b046c6aa0fd24318b227e8d28992ea6fb9c9
samternent <sam.ternent@gmail.com>
2022-09-18 19:32:18 +0100

    teamconcords

--- src/create.js
+++ src/create.js
@@ -1,4 +1,5 @@
-const { writeFile } = require('fs');
+const fs = require('fs');
+const { dirname } = require('path');
 const { exec } = require('shelljs');
 
 const domain = 'teamconcords.com';
@@ -7,22 +8,34 @@ const subdomain = 'www';
 // configure nginx
 exec(`sudo mkdir -p /var/www/${domain}/html`);
 exec(`sudo chown -R sam:sam /var/www/${domain}/html`);
-writeFile(
+
+function writeFile(path, contents, cb) {
+	fs.mkdir(dirname(path), { recursive: true }, function (err) {
+		if (err) return cb(err);
+
+		fs.writeFile(path, contents, cb);
+	});
+}
+const createStream = writeFile(
 	`/var/www/${domain}/html/index.html`,
-	```
+	`
 <html>
     <head>
-        <title>Welcome to FootballSocial.app!</title>
+        <title>Welcome to ${subdomain}.${domain}!</title>
     </head>
     <body>
-        <h1>Success! The footballsocial.app server block is working!</h1>
+        <h1>Success! The ${subdomain}.${domain} server block is working!</h1>
     </body>
 </html>
-```
+`,
+	(e) => {
+		console.log(e);
+	}
 );
+
 writeFile(
 	`/etc/nginx/sites-available/${domain}`,
-	```
+	`
 server {
   root /var/www/${domain}/html;
   index index.html index.htm index.nginx-debian.html;
@@ -39,7 +52,10 @@ server {
 
   server_name ${domain} ${subdomain}.${domain};
 }
-```
+`,
+	(e) => {
+		console.log(e);
+	}
 );
 
 exec(
