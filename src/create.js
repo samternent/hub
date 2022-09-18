3283d53b58a30599e24016ab3b6b7a8021658d31
samternent <sam.ternent@gmail.com>
2022-09-18 21:07:23 +0100

    errors

--- src/create.js
+++ src/create.js
@@ -5,65 +5,69 @@ const createSSL = require('./ssl.js');
 
 module.exports = function (repo, subdomain, domain) {
 	// configure nginx
-	fs.rmSync(`/var/www/${domain}/html`, { recursive: true, force: true });
-	fs.rmSync(`/etc/nginx/sites-available/${domain}`, {
-		recursive: true,
-		force: true,
-	});
-	fs.rmSync(`/etc/nginx/sites-enabled/${domain}`, {
-		recursive: true,
-		force: true,
-	});
-
-	exec(`sudo mkdir -p /var/www/${domain}/html`);
-	exec(`sudo chown -R sam:sam /var/www/${domain}/html`);
-	exec(`sudo chown -R sam:sam /etc/nginx/sites-available`);
-	exec(`sudo chown -R sam:sam /etc/nginx/sites-enabled`);
-
-	function writeFile(path, contents, cb) {
-		fs.mkdir(dirname(path), { recursive: true }, function (err) {
-			if (err) return cb(err);
-
-			fs.writeFile(path, contents, cb);
+	try {
+		fs.rmSync(`/var/www/${domain}/html`, { recursive: true, force: true });
+		fs.rmSync(`/etc/nginx/sites-available/${domain}`, {
+			recursive: true,
+			force: true,
 		});
-	}
+		fs.rmSync(`/etc/nginx/sites-enabled/${domain}`, {
+			recursive: true,
+			force: true,
+		});
 
-	cd(`~/`);
-	rm('-rf', `/${repo.split('/')[1]}`);
-	exec(`gh repo clone ${repo}`);
-	cd(`~/${repo.split('/')[1]}`);
-	exec('pnpm i');
-	exec('pnpm build');
-	cp('-R', 'dist/', `/var/www/${domain}/html`);
+		exec(`sudo mkdir -p /var/www/${domain}/html`);
+		exec(`sudo chown -R sam:sam /var/www/${domain}/html`);
+		exec(`sudo chown -R sam:sam /etc/nginx/sites-available`);
+		exec(`sudo chown -R sam:sam /etc/nginx/sites-enabled`);
 
-	writeFile(
-		`/etc/nginx/sites-available/${domain}`,
-		`
-server {
-  root /var/www/${domain}/html;
-  index index.html index.htm index.nginx-debian.html;
+		function writeFile(path, contents, cb) {
+			fs.mkdir(dirname(path), { recursive: true }, function (err) {
+				if (err) return cb(err);
 
-  server_name ${domain} ${subdomain}.${domain};
-
-  location / {
-    try_files $uri $uri/ /index.html;
-  }
-}
-server {
-  listen 80;
-  listen [::]:80;
-
-  server_name ${domain} ${subdomain}.${domain};
-}
-`,
-		(e) => {
-			console.log(e);
+				fs.writeFile(path, contents, cb);
+			});
 		}
-	);
 
-	exec(
-		`sudo ln -s /etc/nginx/sites-available/${domain} /etc/nginx/sites-enabled/`
-	);
+		cd(`~/`);
+		rm('-rf', `/${repo.split('/')[1]}`);
+		exec(`gh repo clone ${repo}`);
+		cd(`~/${repo.split('/')[1]}`);
+		exec('pnpm i');
+		exec('pnpm build');
+		cp('-R', 'dist/', `/var/www/${domain}/html`);
 
-	createSSL(subdomain, domain);
+		writeFile(
+			`/etc/nginx/sites-available/${domain}`,
+			`
+  server {
+    root /var/www/${domain}/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name ${domain} ${subdomain}.${domain};
+
+    location / {
+      try_files $uri $uri/ /index.html;
+    }
+  }
+  server {
+    listen 80;
+    listen [::]:80;
+
+    server_name ${domain} ${subdomain}.${domain};
+  }
+  `,
+			(e) => {
+				console.log(e);
+			}
+		);
+
+		exec(
+			`sudo ln -s /etc/nginx/sites-available/${domain} /etc/nginx/sites-enabled/`
+		);
+
+		createSSL(subdomain, domain);
+	} catch (e) {
+		console.log(e);
+	}
 };
