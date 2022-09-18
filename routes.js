bda01283850f2a3cce2939f1041cdfe244d3b593
samternent <sam.ternent@gmail.com>
2022-09-18 17:53:10 +0100

    init

---
+++ routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+	res.send('Welcome to the Webhooks API');
+});
+
+router.post('/deploy', function (req, res) {
+	try {
+		const { repository } = req.body;
+
+		console.log(`cloning ${repository}`);
+	} catch (e) {
+		return res.status(400).send({
+			message: e,
+		});
+	} finally {
+		res.send('Okta Event hook Successfully received');
+	}
+});
+
+module.exports = router;
